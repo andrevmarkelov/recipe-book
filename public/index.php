5a5e034b79d1d65dff37793380a04e8bd638acc8
andrevmarkelov <andrev_markelov@mail.ru>
2024-12-05 15:21:51 +0300

    Добавление начального базового проекта

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Книга рецептов</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <!-- Шапка -->
+    <?php include '../views/layouts/header.php'; ?>
+
+    <!-- Контент -->
+    <div id="content">
+        <div class="container py-5">
+            <h1 class="text-center">Главная страница</h1>
+        </div>
+    </div>
+
+    <!-- Подвал -->
+    <?php include '../views/layouts/footer.php'; ?>
+
+    <!-- Скрипты -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+</body>
+</html>
