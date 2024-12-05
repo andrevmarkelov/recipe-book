b133bbe289a8890a27a37f1fdf9172f422d0dfeb
andrevmarkelov <andrev_markelov@mail.ru>
2024-12-05 18:09:34 +0300

    Добавлена главная страница

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,1 +1,17 @@
-<header class="header">header</header>
+<header class="header">
+    <div class="container py-3">
+        <div class="d-flex justify-content-between align-items-center header-content">
+            <a href="#" class="header-logo">
+                <img src="assets/images/logo.png" alt="Книга рецептов">
+            </a>
+
+            <nav class="header-nav">
+                <ul class="list-unstyled mb-0">
+                    <li><a href="#">Главная</a></li>
+                </ul>
+            </nav>
+
+            <a href="#" class="btn-header">Добавить рецепт</a>
+        </div>
+    </div>
+</header>
